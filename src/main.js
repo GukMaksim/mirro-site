96624e47ab55c1665b666d9bc2648777a27969d7
Maksym Guk <maksim.guk@gmail.com>
2025-05-13 14:39:36 +0300

    feat: add hero section, services section, and update header with cart icon
    - Added a hero section with a background gradient, social icons, and a call-to-action button
    - Created a services section with a grid of service cards, each with an icon and description
    - Updated the header to include a cart icon with a badge and improved styling

--- src/main.js
+++ src/main.js
@@ -1,10 +1,11 @@
-import './style.css'
+import "./style.css";
 
-document.addEventListener('DOMContentLoaded', () => {
-    const toggle = document.getElementById('menu-toggle');
-    const mobileMenu = document.getElementById('mobile-menu');
-  
-    toggle.addEventListener('click', () => {
-      mobileMenu.classList.toggle('hidden');
-    });
+document.addEventListener("DOMContentLoaded", () => {
+  // Мобільне меню
+  const toggle = document.getElementById("menu-toggle");
+  const mobileMenu = document.getElementById("mobile-menu");
+
+  toggle.addEventListener("click", () => {
+    mobileMenu.classList.toggle("hidden");
   });
+});
